3f1181f62ab0856983744084e9d6f9034e781fa7
boiarshinov <artem.borya@gmail.com>
2024-08-15 13:23:22 +0300

    Add step 5

--- src/main/java/dev/boiarshinov/fluentapi/core/step5/App.java
+++ src/main/java/dev/boiarshinov/fluentapi/core/step5/App.java
@@ -1,4 +1,18 @@
 package dev.boiarshinov.fluentapi.core.step5;
 
 public class App {
+
+    //@Bean
+    public ProxyConfiguration proxyConfig() {
+        return ProxyConfiguration.configure()
+            .recipients("host1:port", "host2:port")
+            .loadBalancer(lb ->
+                lb.roundRobin()
+            )
+            .acl("192.168.0.1", "192.168.0.2")
+            .declineOnContent("Тинькофф")
+            .replaceResponse("Тинькофф банк", "Т-Банк")
+            .replaceResponse("Олег", "Александр Поломодов")
+            .build();
+    }
 }
