3e978f5b86ed9921f740e3d7e72ada4f5e22cff4
boiarshinov <artem.borya@gmail.com>
2024-08-25 20:45:41 +0300

    Add readme

---
+++ security-demo/readme.md
@@ -0,0 +1,3 @@
+# Пример Fluent API Spring Security
+
+Представлен пример того, как развивалось Fluent API конфигурации Spring Security.
--- src/main/java/dev/boiarshinov/fluentapi/core/step4/package-info.java
+++ src/main/java/dev/boiarshinov/fluentapi/core/step4/package-info.java
@@ -1,4 +1,5 @@
 /**
+ * Приём "Context Change" - смена контекста.
  * Добавляем возможность войти и выйти из уточняющей конфигурации. Делаем метод loadBalance()
  */
 package dev.boiarshinov.fluentapi.core.step4;
